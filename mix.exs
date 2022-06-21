0bf5a8238800a988f5dbc2e8886d9f8041de623a
nelsonkopliku <nelson.kopliku@suse.com>
2022-06-22 00:13:14 +0200

    added jason to the mix

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Wanda.MixProject do
   defp deps do
     [
       {:elixir_uuid, "~> 1.2"},
-      {:yaml_elixir, "~> 2.9"}
+      {:yaml_elixir, "~> 2.9"},
+      {:jason, "~> 1.3"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
