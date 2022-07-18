f202bba1da330dbaac539c567f634c9e8e190b6e
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-07-18 15:42:46 +0200

    Refactor execution supervisor
    Co-authored-by: Carmine Di Monaco <carmine.dimonaco@suse.com>

--- lib/wanda/execution_supervisor.ex
+++ lib/wanda/execution_supervisor.ex
@@ -2,23 +2,27 @@ defmodule Wanda.ExecutionSupervisor do
   @moduledoc nil
 
   use DynamicSupervisor
-  alias Wanda.ExecutionServer
+
+  alias Wanda.Execution
 
   @spec start_link(any) :: :ignore | {:error, any} | {:ok, pid}
   def start_link(_arg) do
     DynamicSupervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
-  def start_child(execution_id) do
-    # Shorthand to retrieve the child specification from the `child_spec/1` method of the given module.
-    child_specification = {ExecutionServer, execution_id}
+  def start_execution(execution_id, group_id, targets) do
+    child_spec = %{
+      id: {Execution, execution_id},
+      start: {Execution, :start_link, [execution_id, group_id, targets]},
+      restart: :temporary,
+      type: :worker
+    }
 
-    DynamicSupervisor.start_child(__MODULE__, child_specification)
+    DynamicSupervisor.start_child(__MODULE__, child_spec)
   end
 
   @impl true
-  def init(_arg) do
-    # :one_for_one strategy: if a child process crashes, only that process is restarted.
+  def init(_args) do
     DynamicSupervisor.init(strategy: :one_for_one)
   end
 end
