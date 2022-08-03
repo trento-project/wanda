642343cb64e88285da98ac7b47cac8059ed85997
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-08-03 06:51:43 +0200

    Add expectation struct and get_expectation function
    Co-authored-by: Carmine Di Monaco  <carmine.dimonaco@suse.com>

--- lib/wanda/catalog.ex
+++ lib/wanda/catalog.ex
@@ -3,24 +3,46 @@ defmodule Wanda.Catalog do
   Function to interact with the checks catalog.
   """
 
+  alias Wanda.Catalog.Expectation
+
   @catalog_path Application.compile_env!(:wanda, [__MODULE__, :catalog_path])
 
-  def get_facts_names(check_id) do
-    check_id
-    |> get_check()
-    |> Map.get("facts")
-    |> Enum.map(& &1["name"])
-  end
-
+  @doc """
+  Get a list of expectations for a given check.
+  """
+  @spec get_expectations(String.t()) :: [Expectation.t()]
   def get_expectations(check_id) do
     check_id
     |> get_check()
     |> Map.fetch!("expectations")
+    |> Enum.map(&map_expectation/1)
+  end
+
+  def get_expectation(check_id, name) do
+    check_id
+    |> get_expectations()
+    |> Enum.find(&(&1.name == name))
   end
 
   defp get_check(check_id) do
     @catalog_path
     |> Path.join("#{check_id}.yaml")
     |> YamlElixir.read_from_file!()
+  end
+
+  defp map_expectation(%{"name" => name, "expect" => expression}) do
+    %Expectation{
+      name: name,
+      type: :expect,
+      expression: expression
+    }
+  end
+
+  defp map_expectation(%{"name" => name, "expect_same" => expression}) do
+    %Expectation{
+      name: name,
+      type: :expect_same,
+      expression: expression
+    }
   end
 end
