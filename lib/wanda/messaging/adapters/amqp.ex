d71c315b286f4745e09f965d6f82e0d5f6b5ab65
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-08-05 15:58:02 +0200

    Add AMQP adapter, consumer and publisher

--- lib/wanda/messaging/adapters/amqp.ex
+++ lib/wanda/messaging/adapters/amqp.ex
@@ -5,6 +5,16 @@ defmodule Wanda.Messaging.Adapters.AMQP do
 
   @behaviour Wanda.Messaging.Adapters.Behaviour
 
+  alias Wanda.Messaging.Adapters.AMQP.Publisher
+
   @impl true
-  def publish(_message), do: :ok
+  def publish(routing_key, message), do: Publisher.publish_message(message, routing_key)
+
+  @impl true
+  def child_spec do
+    [
+      Wanda.Messaging.Adapters.AMQP.Consumer,
+      Wanda.Messaging.Adapters.AMQP.Publisher
+    ]
+  end
 end
