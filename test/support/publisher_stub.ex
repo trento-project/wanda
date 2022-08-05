b9e1a70bd87cb4b7198e20484e86047aebd32cd5
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-08-05 16:14:18 +0200

    Update stubs

--- test/support/publisher_stub.ex
+++ test/support/publisher_stub.ex
@@ -4,5 +4,8 @@ defmodule Wanda.Support.PublisherStub do
   @behaviour Wanda.Messaging.Adapters.Behaviour
 
   @impl true
-  def publish(_), do: :ok
+  def publish(_, _), do: :ok
+
+  @impl true
+  def child_spec, do: []
 end
