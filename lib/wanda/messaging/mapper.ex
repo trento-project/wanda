cb2127eb03e84207832c918635151353a3c42971
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-08-09 15:26:38 +0200

    Add messaging mapper

---
+++ lib/wanda/messaging/mapper.ex
@@ -0,0 +1,16 @@
+defmodule Wanda.Messaging.Mapper do
+  @moduledoc """
+  Maps messages to events.
+  """
+  alias Cloudevents.Format.V_1_0.Event, as: CloudEvent
+
+  alias Wanda.JsonSchema
+
+  @spec from_json(binary()) :: {:ok, CloudEvent.t()} | {:error, any}
+  def from_json(json) do
+    with {:ok, %CloudEvent{data: data, type: type} = cloud_event} <- Cloudevents.from_json(json),
+         :ok <- JsonSchema.validate(type, data) do
+      {:ok, cloud_event}
+    end
+  end
+end
