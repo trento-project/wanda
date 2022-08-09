05768ef728689f22f35f7dd3478c34084b4395fc
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-08-09 15:25:12 +0200

    add policy test

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,6 +1,8 @@
 Mox.defmock(Wanda.Messaging.Adapters.Mock, for: Wanda.Messaging.Adapters.Behaviour)
-
 Application.put_env(:wanda, :messaging, adapter: Wanda.Messaging.Adapters.Mock)
+
+Mox.defmock(Wanda.Execution.Mock, for: Wanda.Execution.Behaviour)
+Application.put_env(:wanda, Wanda.Policy, execution_impl: Wanda.Execution.Mock)
 
 ExUnit.start(capture_log: true)
 Faker.start()
