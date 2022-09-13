46814ef91a197eb64b77c6218c1f86ff6df25c42
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-09-13 18:36:23 +0200

    Refactor policy with contracts

--- test/policy_test.exs
+++ test/policy_test.exs
@@ -3,7 +3,11 @@ defmodule Wanda.PolicyTest do
 
   import Mox
 
-  alias Cloudevents.Format.V_1_0.Event, as: CloudEvent
+  alias Trento.Checks.V1.{
+    ExecutionRequested,
+    FactsGathered
+  }
+
   alias Wanda.Execution.{Fact, Target}
 
   setup :verify_on_exit!
@@ -12,7 +16,6 @@ defmodule Wanda.PolicyTest do
     execution_id = UUID.uuid4()
     group_id = UUID.uuid4()
     agent_id = UUID.uuid4()
-    targets = [%{"agent_id" => agent_id, "checks" => ["check_id"]}]
 
     expect(Wanda.Execution.Mock, :start_execution, fn ^execution_id,
                                                       ^group_id,
@@ -26,22 +29,18 @@ defmodule Wanda.PolicyTest do
     end)
 
     assert :ok =
-             Wanda.Policy.handle_event(%CloudEvent{
-               id: UUID.uuid4(),
-               source: "trento.wanda",
-               type: "trento.checks.v1.ExecutionRequested",
-               data: %{
-                 "execution_id" => execution_id,
-                 "group_id" => group_id,
-                 "targets" => targets
-               }
-             })
+             %{
+               execution_id: execution_id,
+               group_id: group_id,
+               targets: [%{agent_id: agent_id, checks: ["check_id"]}]
+             }
+             |> ExecutionRequested.new!()
+             |> Wanda.Policy.handle_event()
   end
 
   test "should handle a FactsGathered event" do
     execution_id = UUID.uuid4()
     agent_id = UUID.uuid4()
-    facts = [%{"check_id" => "check_id", "name" => "name", "value" => "value"}]
 
     expect(Wanda.Execution.Mock, :receive_facts, fn ^execution_id,
                                                     ^agent_id,
@@ -56,21 +55,12 @@ defmodule Wanda.PolicyTest do
     end)
 
     assert :ok =
-             Wanda.Policy.handle_event(%CloudEvent{
-               id: UUID.uuid4(),
-               source: "trento.wanda",
-               type: "trento.checks.v1.FactsGathered",
-               data: %{"execution_id" => execution_id, "agent_id" => agent_id, "facts" => facts}
-             })
-  end
-
-  test "should return error if an unknown event is handled" do
-    assert {:error, :unsupported_event} =
-             Wanda.Policy.handle_event(%CloudEvent{
-               id: UUID.uuid4(),
-               source: "trento.wanda",
-               type: Faker.StarWars.quote(),
-               data: %{}
-             })
+             %{
+               execution_id: execution_id,
+               agent_id: agent_id,
+               facts_gathered: [%{check_id: "check_id", name: "name", value: "value"}]
+             }
+             |> FactsGathered.new!()
+             |> Wanda.Policy.handle_event()
   end
 end
