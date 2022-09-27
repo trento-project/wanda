24688ddecf0dbc9be02eae4124ea771ece9b373d
Alessio Biancalana <alessio.biancalana@suse.com>
2022-09-27 15:48:52 +0200

    Add alias to data_case.ex as recommended by Credo

--- test/support/data_case.ex
+++ test/support/data_case.ex
@@ -15,6 +15,7 @@ defmodule Wanda.DataCase do
   """
 
   use ExUnit.CaseTemplate
+  alias Ecto.Adapters.SQL.Sandbox
 
   using do
     quote do
@@ -36,8 +37,8 @@ defmodule Wanda.DataCase do
   Sets up the sandbox based on the test tags.
   """
   def setup_sandbox(tags) do
-    pid = Ecto.Adapters.SQL.Sandbox.start_owner!(Wanda.Repo, shared: not tags[:async])
-    on_exit(fn -> Ecto.Adapters.SQL.Sandbox.stop_owner(pid) end)
+    pid = Sandbox.start_owner!(Wanda.Repo, shared: not tags[:async])
+    on_exit(fn -> Sandbox.stop_owner(pid) end)
   end
 
   @doc """
