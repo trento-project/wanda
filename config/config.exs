dd53feff95c684b34d5ab7bf243e91d23bacd4eb
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-09-27 09:37:46 +0200

    Refactor config

--- config/config.exs
+++ config/config.exs
@@ -5,20 +5,19 @@ config :wanda, Wanda.Messaging.Publisher, adapter: Wanda.Messaging.Adapters.AMQP
 config :wanda,
   children: [Wanda.Messaging.Adapters.AMQP.Publisher, Wanda.Messaging.Adapters.AMQP.Consumer]
 
-config :wanda, :messaging,
-  adapter: Wanda.Messaging.Adapters.AMQP,
-  amqp: [
-    consumer: [
-      queue: "trento.checks.executions",
-      exchange: "trento.checks",
-      routing_key: "executions",
-      prefetch_count: "10",
-      connection: "amqp://wanda:wanda@localhost:5672"
-    ],
-    publisher: [
-      exchange: "trento.checks",
-      connection: "amqp://wanda:wanda@localhost:5672"
-    ]
+config :wanda, :messaging, adapter: Wanda.Messaging.Adapters.AMQP
+
+config :wanda, Wanda.Messaging.Adapters.AMQP,
+  consumer: [
+    queue: "trento.checks.executions",
+    exchange: "trento.checks",
+    routing_key: "executions",
+    prefetch_count: "10",
+    connection: "amqp://wanda:wanda@localhost:5672"
+  ],
+  publisher: [
+    exchange: "trento.checks",
+    connection: "amqp://wanda:wanda@localhost:5672"
   ]
 
 config :wanda, Wanda.Catalog, catalog_path: "priv/catalog"
