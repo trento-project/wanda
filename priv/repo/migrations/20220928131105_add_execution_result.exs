69c4849ac7add66888b393ec8b31263e0a24d9a6
Nelson Kopliku <nelson.kopliku@suse.com>
2022-09-28 17:00:26 +0200

    Added a storable ExecutionResult entity and database migration

---
+++ priv/repo/migrations/20220928131105_add_execution_result.exs
@@ -0,0 +1,16 @@
+defmodule Wanda.Repo.Migrations.AddExecutionResult do
+  use Ecto.Migration
+
+  def change do
+    create table(:execution_results, primary_key: false) do
+      add :execution_id, :uuid, primary_key: true
+      add :group_id, :uuid, null: false
+      add :payload, :map, null: false
+
+      timestamps()
+    end
+
+    create index(:execution_results, [:group_id])
+    create unique_index(:execution_results, [:execution_id, :group_id])
+  end
+end
