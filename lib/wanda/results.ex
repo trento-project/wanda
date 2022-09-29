aee6045a8f097b0ad2f6debae4b684f14cbf4c04
Nelson Kopliku <nelson.kopliku@suse.com>
2022-09-29 16:34:32 +0200

    Implement the execution results append funcionality

---
+++ lib/wanda/results.ex
@@ -0,0 +1,24 @@
+defmodule Wanda.Results do
+  @moduledoc """
+  This module exposes functionalities to interact with the historycal log of ExecutionResults.
+  It allows to:
+   - append an item to the log
+  """
+
+  alias Wanda.Execution.{ExecutionResult, Result}
+
+  @spec append(Result.t()) :: {:ok, any() } | {:error, any()}
+  def append(
+        %Result{
+          execution_id: execution_id,
+          group_id: group_id
+        } = result
+      ) do
+    %ExecutionResult{
+      execution_id: execution_id,
+      group_id: group_id,
+      payload: result
+    }
+    |> Wanda.Repo.insert()
+  end
+end
