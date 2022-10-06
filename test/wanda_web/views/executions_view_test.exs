7e60cda4766fea9bbfcce9f6c31580fcff11dfc1
Alessio Biancalana <alessio.biancalana@suse.com>
2022-10-06 12:43:37 +0200

    Add test on the view side

---
+++ test/wanda_web/views/executions_view_test.exs
@@ -0,0 +1,31 @@
+defmodule WandaWeb.ExecutionsViewTest do
+  use WandaWeb.ConnCase, async: true
+
+  import Phoenix.View
+  import Wanda.Factory
+
+  alias Wanda.Results.ExecutionResult
+
+  describe "ExecutionsView" do
+    test "renders list_checks_executions.json" do
+      [
+        %ExecutionResult{execution_id: execution_1, group_id: group_1},
+        %ExecutionResult{execution_id: execution_2, group_id: group_2},
+        %ExecutionResult{execution_id: execution_3, group_id: group_3}
+      ] = data = build_list(3, :execution_result)
+
+      assert %{
+               data: [
+                 %ExecutionResult{execution_id: ^execution_1, group_id: ^group_1},
+                 %ExecutionResult{execution_id: ^execution_2, group_id: ^group_2},
+                 %ExecutionResult{execution_id: ^execution_3, group_id: ^group_3}
+               ],
+               total_count: 3
+             } =
+               render(WandaWeb.ExecutionsView, "list_checks_executions.json",
+                 data: data,
+                 total_count: length(data)
+               )
+    end
+  end
+end
