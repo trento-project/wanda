1f31107ea2402a836ae3aee365c52f7a657181fb
arbulu89 <xarbulu@suse.com>
2022-10-11 15:00:11 +0200

    Remove ssl configuration from endpoint

--- config/runtime.exs
+++ config/runtime.exs
@@ -34,11 +34,9 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :wanda, WandaWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
