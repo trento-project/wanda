8c8dc4ed95e8aebdc9c0d409004808182d087abb
Fabrizio Sestito <fabrizio.sestito@suse.com>
2022-10-12 21:34:40 +0200

    Rename controller actions

--- lib/wanda_web/controllers/catalog_controller.ex
+++ lib/wanda_web/controllers/catalog_controller.ex
@@ -5,13 +5,13 @@ defmodule WandaWeb.CatalogController do
   alias Wanda.Catalog
   alias WandaWeb.Schemas.CatalogResponse
 
-  operation :list_catalog,
+  operation :catalog,
     summary: "List checks catalog",
     responses: [
       ok: {"Check catalog response", "application/json", CatalogResponse}
     ]
 
-  def list_catalog(conn, _) do
+  def catalog(conn, _) do
     catalog = Catalog.get_catalog()
     render(conn, catalog: catalog)
   end
