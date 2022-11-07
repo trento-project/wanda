ac71fb5812d0046f69e38da851ec112d19630009
arbulu89 <xarbulu@suse.com>
2022-11-07 10:24:09 +0100

    Handle CORS in dev environment

--- lib/wanda_web/endpoint.ex
+++ lib/wanda_web/endpoint.ex
@@ -29,6 +29,10 @@ defmodule WandaWeb.Endpoint do
     plug Phoenix.Ecto.CheckRepoStatus, otp_app: :wanda
   end
 
+  if Mix.env() == :dev do
+    plug CORSPlug
+  end
+
   plug Plug.RequestId
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
