f6bedb40c18880d06c46e1624cf2440a4d3b4b9b
Nelson Kopliku <nelson.kopliku@suse.com>
2022-11-11 22:30:52 +0100

    Wire start execution action to API endpoint

--- lib/wanda_web/router.ex
+++ lib/wanda_web/router.ex
@@ -12,6 +12,7 @@ defmodule WandaWeb.Router do
     pipe_through :api
 
     resources "/executions", ExecutionController, only: [:index, :show]
+    post "/executions/start", ExecutionController, :start
     get "/catalog", CatalogController, :catalog
   end
 
