4d6768c11289aa6b8bb722ef340b8fbe580e6477
Nelson Kopliku <nelson.kopliku@suse.com>
2022-12-01 09:21:02 +0100

    Add FactError OpenApi schema

---
+++ lib/wanda_web/schemas/execution/fact_error.ex
@@ -0,0 +1,20 @@
+defmodule WandaWeb.Schemas.ExecutionResponse.FactError do
+  @moduledoc false
+
+  alias OpenApiSpex.Schema
+
+  require OpenApiSpex
+
+  OpenApiSpex.schema(%{
+    title: "FactError",
+    description: "An error describing that a fact could not be gathered",
+    type: :object,
+    properties: %{
+      check_id: %Schema{type: :string, description: "Check ID"},
+      name: %Schema{type: :string, description: "Fact name"},
+      type: %Schema{type: :string, description: "Error type"},
+      message: %Schema{type: :string, description: "Error message"}
+    },
+    required: [:check_id, :name, :type, :message]
+  })
+end
