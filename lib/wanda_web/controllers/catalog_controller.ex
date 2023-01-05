9fff6c805b55a7c5d3849fb6a4338b146bf4cea1
Alessio Biancalana <alessio.biancalana@suse.com>
2023-01-05 15:44:59 +0100

    Update catalog controller to use params as env

--- lib/wanda_web/controllers/catalog_controller.ex
+++ lib/wanda_web/controllers/catalog_controller.ex
@@ -5,14 +5,19 @@ defmodule WandaWeb.CatalogController do
   alias Wanda.Catalog
   alias WandaWeb.Schemas.CatalogResponse
 
+  plug OpenApiSpex.Plug.CastAndValidate, json_render_error_v2: true
+
   operation :catalog,
     summary: "List checks catalog",
+    parameters: [
+      provider: [in: :query, type: :string, description: "cloud provider"]
+    ],
     responses: [
       ok: {"Check catalog response", "application/json", CatalogResponse}
     ]
 
-  def catalog(conn, _) do
-    catalog = Catalog.get_catalog()
+  def catalog(conn, params) do
+    catalog = Catalog.get_catalog(params)
     render(conn, catalog: catalog)
   end
 end
