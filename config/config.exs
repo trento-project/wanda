c50073a9ba2396caf83c78b71d1e0be41a4fc9c3
Fabrizio Sestito <fabrizio.sestito@suse.com>
2023-01-16 18:07:45 +0100

    Disable rhai_rustler precompilation

--- config/config.exs
+++ config/config.exs
@@ -47,6 +47,9 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# Disable rustler precompiled NIFs
+config :rustler_precompiled, :force_build, rhai_rustler: true
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
