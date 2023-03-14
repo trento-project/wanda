9216dbf00355c265dd568578dfb29b68c61dbb99
Fabrizio Sestito <fabrizio.sestito@suse.com>
2023-03-14 13:13:36 +0100

    Refactor ErrorView

--- test/wanda_web/views/error_view_test.exs
+++ test/wanda_web/views/error_view_test.exs
@@ -4,12 +4,23 @@ defmodule WandaWeb.ErrorViewTest do
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
-  test "renders 404.json" do
-    assert render(WandaWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+  test "renders 422.json" do
+    assert render(WandaWeb.ErrorView, "422.json", reason: "Invalid values.") == %{
+             errors: [%{detail: "Invalid values.", title: "Unprocessable Entity"}]
+           }
   end
 
-  test "renders 500.json" do
-    assert render(WandaWeb.ErrorView, "500.json", []) ==
-             %{errors: %{detail: "Internal Server Error"}}
+  test "renders 404.json" do
+    assert render(WandaWeb.ErrorView, "404.json", []) == %{
+             errors: [
+               %{detail: "The requested resource was not found.", title: "Not Found"}
+             ]
+           }
+  end
+
+  test "renders a generic error" do
+    assert render(WandaWeb.ErrorView, "418.json", []) == %{
+             errors: [%{detail: "An error has occurred.", title: "I'm a teapot"}]
+           }
   end
 end
