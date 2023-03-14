4fd6cd41b57df5edeb2271d20ad07618bba4d55d
Fabrizio Sestito <fabrizio.sestito@suse.com>
2023-03-14 13:13:13 +0100

    Add error schemas

---
+++ lib/wanda_web/schemas/bad_request.ex
@@ -0,0 +1,37 @@
+defmodule WandaWeb.Schemas.BadRequest do
+  @moduledoc """
+  Bad Request
+  """
+  alias OpenApiSpex.Operation
+  alias OpenApiSpex.Schema
+
+  require OpenApiSpex
+
+  OpenApiSpex.schema(%{
+    title: "BadRequest",
+    type: :object,
+    properties: %{
+      errors: %Schema{
+        type: :array,
+        items: %Schema{
+          type: :object,
+          properties: %{
+            detail: %Schema{
+              type: :string,
+              example: "Invalid request payload."
+            },
+            title: %Schema{type: :string, example: "Bad Request"}
+          }
+        }
+      }
+    }
+  })
+
+  def response do
+    Operation.response(
+      "Bad Request",
+      "application/json",
+      __MODULE__
+    )
+  end
+end
