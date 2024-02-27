c2e9331e780cbe9d328ee852d703ba76b2930653
arbulu89 <xarbulu@suse.com>
2024-02-27 11:53:51 +0100

    Remove duplicated schemas and view functions

--- lib/wanda_web/views/v2/catalog_view.ex
+++ lib/wanda_web/views/v2/catalog_view.ex
@@ -1,21 +1,7 @@
 defmodule WandaWeb.V2.CatalogView do
   use WandaWeb, :view
 
-  alias Wanda.Catalog.Check
-
   def render("catalog.json", %{catalog: catalog}) do
-    %{items: render_many(catalog, WandaWeb.V2.CatalogView, "check.json", as: :check)}
-  end
-
-  def render("check.json", %{check: %Check{} = check}), do: adapt_v2(check)
-
-  def adapt_v2(%{expectations: expectations} = check) do
-    adapted_expectations =
-      Enum.map(expectations, fn
-        %{type: :expect_enum} = expectation -> Map.put(expectation, :type, :unknown)
-        expectation -> expectation
-      end)
-
-    %{check | expectations: adapted_expectations}
+    %{items: render_many(catalog, WandaWeb.V1.CatalogView, "check.json", as: :check)}
   end
 end
