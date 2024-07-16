b9932405bd07628129ad94cb1caf6d5a084db312
Stefano Torresi <stefano@torresi.io>
2024-07-16 18:48:56 +0200

    fix get_version_from_git script failing when there are no tags

--- hack/get_version_from_git.sh
+++ hack/get_version_from_git.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-TAG=$( git tag | grep -E "[0-9]\.[0-9]\.[0-9]" | sort -rn | head -n1 )
+TAG=$( git tag | (grep -E "[0-9]\.[0-9]\.[0-9]" || true) | sort -rn | head -n1 )
+COMMIT_SHA=$(git show -s --format=%ct.%h HEAD)
 
 if [ -n "${TAG}" ]; then
   COMMITS_SINCE_TAG=$(git rev-list "${TAG}".. --count)
   if [ "${COMMITS_SINCE_TAG}" -gt 0 ]; then
-    COMMIT_SHA=$(git show -s --format=%ct.%h HEAD)
     SUFFIX="+git.dev${COMMITS_SINCE_TAG}.${COMMIT_SHA}"
   fi
 else
-  TAG="0"
+  TAG="0.0.0"
+  SUFFIX="+git.dev.${COMMIT_SHA}"
 fi
 
 echo "${TAG}${SUFFIX}"
