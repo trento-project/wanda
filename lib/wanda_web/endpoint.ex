6c4ef7380220cd3dbee30fea93524df97c68d026
EMaksy <emaksymenko@suse.com>
2024-09-27 17:37:40 +0200

    Remove Phoenix.View from configs

--- lib/wanda_web/endpoint.ex
+++ lib/wanda_web/endpoint.ex
@@ -1,5 +1,6 @@
 defmodule WandaWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :wanda
+  alias WandaWeb
 
   # The session will be stored in the cookie and signed,
   # this means its contents can be read but not tampered with.
@@ -20,7 +21,7 @@ defmodule WandaWeb.Endpoint do
     at: "/",
     from: :wanda,
     gzip: false,
-    only: ~w(assets fonts images favicon.ico robots.txt)
+    only: WandaWeb.static_paths()
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
