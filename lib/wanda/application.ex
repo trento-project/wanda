648278360145e6ee42ad42faaed12d1759dc8744
arbulu89 <xarbulu@suse.com>
2025-01-08 15:36:56 +0100

    Start dynamic server in application bootup

--- lib/wanda/application.ex
+++ lib/wanda/application.ex
@@ -17,7 +17,8 @@ defmodule Wanda.Application do
         WandaWeb.Endpoint,
         # Start a worker by calling: Wanda.Worker.start_link(arg)
         # {Wanda.Worker, arg}
-        {DynamicSupervisor, strategy: :one_for_one, name: Wanda.Executions.Supervisor}
+        {DynamicSupervisor, strategy: :one_for_one, name: Wanda.Executions.Supervisor},
+        {DynamicSupervisor, strategy: :one_for_one, name: Wanda.Operations.Supervisor}
       ] ++ Application.get_env(:wanda, :children, [])
 
     # See https://hexdocs.pm/elixir/Supervisor.html
