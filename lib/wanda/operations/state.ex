e61874d1e01141140df6d411e7c619d242722eca
arbulu89 <xarbulu@suse.com>
2025-01-13 10:08:34 +0100

    Document state fields

--- lib/wanda/operations/state.ex
+++ lib/wanda/operations/state.ex
@@ -1,6 +1,17 @@
 defmodule Wanda.Operations.State do
   @moduledoc """
   State of an operation.
+
+  The state is composed by the next field:
+  - engine: RHAI engine used to run predicate validations
+  - operation_id: Identifier of this operation execution
+  - group_id: Identifier of the group or resource that is under the operation.
+              For example, this can be the identifier of an individual host or a cluster.
+  - operation: Operation to execute
+  - timeout: Timeout to stop the operation
+  - pending_targets_on_step: Target ids which are pending to report back for the current operation step
+  - current_step_index: Current step index being executed
+  - step_failed: Whether the current step has failed in any of the targets executing it
   """
 
   alias Wanda.Operations.{OperationTarget, StepReport}
