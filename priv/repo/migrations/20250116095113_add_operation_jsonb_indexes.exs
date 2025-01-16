f56ff44b2a13ec89bc97f617426ada602df701ec
arbulu89 <xarbulu@suse.com>
2025-01-16 12:10:57 +0100

    Update index migration using ecto and adding jsonb_path_ops

--- priv/repo/migrations/20250116095113_add_operation_jsonb_indexes.exs
+++ priv/repo/migrations/20250116095113_add_operation_jsonb_indexes.exs
@@ -1,13 +1,8 @@
 defmodule Wanda.Repo.Migrations.AddOperationJsonbIndexes do
   use Ecto.Migration
 
-  def up do
-    execute("CREATE INDEX operation_targets ON operations USING GIN(targets)")
-    execute("CREATE INDEX operation_agent_reports ON operations USING GIN(agent_reports)")
-  end
-
-  def down do
-    execute("DROP INDEX operation_targets")
-    execute("DROP INDEX operation_agent_reports")
+  def change do
+    create index(:operations, ["targets jsonb_path_ops"], using: "GIN")
+    create index(:operations, ["agent_reports jsonb_path_ops"], using: "GIN")
   end
 end
