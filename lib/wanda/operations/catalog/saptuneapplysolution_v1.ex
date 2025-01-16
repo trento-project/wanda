47a413c427baa31ae02fb2476cc274431a4bd474
arbulu89 <xarbulu@suse.com>
2025-01-16 15:42:14 +0100

    Fix wording in operation description

--- lib/wanda/operations/catalog/saptuneapplysolution_v1.ex
+++ lib/wanda/operations/catalog/saptuneapplysolution_v1.ex
@@ -6,7 +6,7 @@ defmodule Wanda.Operations.Catalog.SaptuneApplySolutionV1 do
       id: "saptuneapplysolution@v1",
       name: "Apply saptune solution",
       description: """
-      This solution applies a saptune solution in the targets.
+      This operation applies a saptune solution in the targets.
       """,
       required_args: ["solution"],
       steps: [
