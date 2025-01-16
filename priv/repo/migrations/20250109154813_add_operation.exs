0f1cd9aa2c951102bc31b679a7816cb5f146d0ef
arbulu89 <xarbulu@suse.com>
2025-01-16 10:22:08 +0100

    Use jsonb specifially as operation schema type

--- priv/repo/migrations/20250109154813_add_operation.exs
+++ priv/repo/migrations/20250109154813_add_operation.exs
@@ -7,8 +7,8 @@ defmodule Wanda.Repo.Migrations.AddOperation do
       add :group_id, :uuid, null: false
       add :result, :string, null: false
       add :status, :string, null: false
-      add :targets, :map, null: false, default: "[]"
-      add :agent_reports, :map, null: false, default: "[]"
+      add :targets, :jsonb, null: false, default: "[]"
+      add :agent_reports, :jsonb, null: false, default: "[]"
       add :completed_at, :utc_datetime_usec
       timestamps(type: :utc_datetime_usec, inserted_at: :started_at)
     end
