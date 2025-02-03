cad35dccb1e12d6defbb34b2157201e2c754b007
Nelson Kopliku <nelson.kopliku@suse.com>
2025-02-03 17:42:46 +0100

    Add recursive key atomization

--- lib/wanda/support/ecto/json.ex
+++ lib/wanda/support/ecto/json.ex
@@ -19,10 +19,12 @@ defmodule Wanda.Support.Ecto.Json do
   def dump(_), do: :error
 
   defp atomize_map_keys(map) when is_map(map) do
-    Map.new(map, fn {k, v} -> {String.to_existing_atom(k), v} end)
+    Map.new(map, fn {k, v} -> {String.to_existing_atom(k), atomize_map_keys(v)} end)
   end
 
   defp atomize_map_keys(map) when is_list(map) do
     Enum.map(map, &atomize_map_keys/1)
   end
+
+  defp atomize_map_keys(value), do: value
 end
