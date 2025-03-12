c4e3cc758f7be06857cece6382440b08957fd762
Nelson Kopliku <nelson.kopliku@suse.com>
2025-03-12 09:05:26 +0100

    Add a catalog evaluation

---
+++ lib/wanda/catalog/resolved_value.ex
@@ -0,0 +1,26 @@
+defmodule Wanda.Catalog.ResolvedValue do
+  @moduledoc """
+  Represents a resolved check value as defined check specification.
+  It is based on the contextual environment.
+  """
+  alias Wanda.Catalog.Value
+
+  @type value :: boolean() | number() | String.t()
+
+  @derive Jason.Encoder
+  defstruct [
+    :spec,
+    :name,
+    :original_value,
+    :custom_value,
+    :customized
+  ]
+
+  @type t :: %__MODULE__{
+          spec: Value.t(),
+          name: String.t(),
+          original_value: value(),
+          custom_value: value(),
+          customized: boolean()
+        }
+end
