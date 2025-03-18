5af2b43d4a03ff4e29f869edd137d2c26c9f4756
arbulu89 <xarbulu@suse.com>
2025-03-18 09:19:18 +0100

    Update api schema to return new fields

--- lib/wanda_web/schemas/v1/operation/agent_report.ex
+++ lib/wanda_web/schemas/v1/operation/agent_report.ex
@@ -15,9 +15,25 @@ defmodule WandaWeb.Schemas.V1.Operation.AgentReport do
       additionalProperties: false,
       properties: %{
         agent_id: %Schema{type: :string, format: :uuid, description: "Agent ID"},
-        result: %Schema{type: :string, enum: Result.values()}
+        result: %Schema{type: :string, enum: Result.values()},
+        diff: %Schema{
+          type: :object,
+          additionalProperties: false,
+          nullable: true,
+          properties: %{
+            before: %Schema{description: "The state before applying the operation"},
+            after: %Schema{description: "The state after applying the operation"}
+          },
+          description: "The operation before/after difference",
+          required: [:before, :after]
+        },
+        error_message: %Schema{
+          type: :string,
+          nullable: true,
+          description: "Error message in case of a failed operation"
+        }
       },
-      required: [:agent_id, :result]
+      required: [:agent_id, :result, :diff, :error_message]
     },
     struct?: false
   )
