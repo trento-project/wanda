c50de96270461da8bf8389a28668f7fdeea95737
arbulu89 <xarbulu@suse.com>
2025-07-17 11:34:03 +0200

    Add sap system start/stop operations

---
+++ lib/wanda/operations/catalog/sapsystemstop_v1.ex
@@ -0,0 +1,25 @@
+defmodule Wanda.Operations.Catalog.SapSystemStopV1 do
+  @moduledoc false
+
+  use Wanda.Operations.Catalog.Operation,
+    operation: %Wanda.Operations.Catalog.Operation{
+      id: "sapsystemstop@v1",
+      name: "Stop SAP system",
+      description: """
+      This operation stops a complete SAP system.
+
+      Arguments:
+        instance_number (string): Instance number to stop the system
+        instance_type (string): Instance type to stop. Available values: all|abap|j2ee|scs|enqrep. Default value: all
+        timeout (number): Timeout in seconds to wait until the system is stopped
+      """,
+      required_args: ["instance_number"],
+      steps: [
+        %Wanda.Operations.Catalog.Step{
+          name: "Stop SAP system",
+          operator: "sapsystemstop@v1",
+          predicate: "*"
+        }
+      ]
+    }
+end
