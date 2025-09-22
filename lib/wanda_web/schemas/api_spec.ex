1938a88ec622b98f8785ed34e7d8865e232d6a30
Antonio Gámez, PhD <antonio.gamez@suse.com>
2025-09-22 14:28:59 +0200

    [TRNT-3845] Combine API specs in a single file with improved feedback loop (#634)
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>

--- lib/wanda_web/schemas/api_spec.ex
+++ lib/wanda_web/schemas/api_spec.ex
@@ -7,6 +7,10 @@ defmodule WandaWeb.Schemas.ApiSpec do
   Example:
     use WandaWeb.OpenApi.ApiSpec,
       api_version: "v1"
+
+    # For unversioned endpoints:
+    use WandaWeb.OpenApi.ApiSpec,
+      api_version: "unversioned"
   """
 
   defmacro __using__(opts) do
@@ -36,14 +40,19 @@ defmodule WandaWeb.Schemas.ApiSpec do
           info: %Info{
             title: "Wanda",
             description: to_string(Application.spec(:wanda, :description)),
-            version: to_string(Application.spec(:wanda, :vsn))
+            version: to_string(Application.spec(:wanda, :vsn)) <> "-" <> unquote(api_version)
           },
           components: %Components{
             securitySchemes: %{"authorization" => %SecurityScheme{type: "http", scheme: "bearer"}}
           },
           security: [%{"authorization" => []}],
           paths: build_paths_for_version(unquote(api_version), router),
-          tags: []
+          tags: [
+            %Tag{
+              name: "Platform",
+              description: "Providing access to Wanda Platform features"
+            }
+          ]
         })
       end
 
@@ -53,14 +62,17 @@ defmodule WandaWeb.Schemas.ApiSpec do
           Server.from_endpoint(Endpoint)
         else
           # If the endpoint is not running, use a placeholder
-          # this happens when generarting openapi.json with --start-app=false
+          # this happens when generating openapi.json with --start-app=false
           # e.g. mix openapi.spec.json --start-app=false --spec WandaWeb.ApiSpec
           %OpenApiSpex.Server{url: "https://demo.trento-project.io"}
         end
       end
 
       defp build_paths_for_version(version, router) do
-        excluded_versions = List.delete(router.available_api_versions(), version)
+        available_versions = router.available_api_versions()
+
+        excluded_versions = List.delete(available_versions, version)
+        actual_versions = List.delete(available_versions, "unversioned")
 
         router
         |> Paths.from_router()
@@ -70,10 +82,18 @@ defmodule WandaWeb.Schemas.ApiSpec do
             |> String.trim("/")
             |> String.split("/")
             |> Enum.at(1)
+            |> map_version(actual_versions)
 
           Enum.member?(excluded_versions, current_version)
         end)
         |> Map.new()
+      end
+
+      defp map_version(version, actual_versions) do
+        case version in actual_versions do
+          true -> version
+          _ -> "unversioned"
+        end
       end
     end
   end
