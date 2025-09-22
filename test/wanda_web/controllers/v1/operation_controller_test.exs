dacb4799a6781c8b0bd405f70f4adb7c1eead634
Antonio Gamez Diaz <antonio.gamez@suse.com>
2025-09-22 15:07:11 +0200

    Fix typo
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>

--- test/wanda_web/controllers/v1/operation_controller_test.exs
+++ test/wanda_web/controllers/v1/operation_controller_test.exs
@@ -30,7 +30,7 @@ defmodule WandaWeb.V1.OperationControllerTest do
       assert_schema(json, "ListOperationsResponse", api_spec)
     end
 
-    test "should return a 422 status code if an invalid paramaters is passed", %{conn: conn} do
+    test "should return a 422 status code if an invalid parameters is passed", %{conn: conn} do
       conn = get(conn, "/api/v1/operations/executions?limit=invalid")
 
       assert 422 == conn.status
