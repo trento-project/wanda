0778e687e15c0c4482353b2a83803bea65cf1b42
Antonio Gamez Diaz <antonio.gamez@suse.com>
2025-10-16 13:47:14 +0200

    Add missing examples
    Signed-off-by: Antonio Gamez Diaz <antonio.gamez@suse.com>

--- lib/wanda_web/schemas/v3/catalog/catalog_response.ex
+++ lib/wanda_web/schemas/v3/catalog/catalog_response.ex
@@ -19,47 +19,45 @@ defmodule WandaWeb.Schemas.V3.Catalog.CatalogResponse do
         items: %Schema{
           type: :array,
           description: "An array containing all catalog checks included in the response.",
-          items: Check
+          items: Check,
+          example: [
+            %{
+              id: "156F64",
+              name: "Corosync `token` timeout",
+              group: "Corosync",
+              description: "Corosync `token` timeout is set to the correct value",
+              remediation:
+                "The value of the Corosync `token` timeout is not set as recommended. Adjust the corosync `token` timeout as recommended...",
+              metadata: %{"category" => "ha", "impact" => "critical"},
+              severity: "critical",
+              facts: [
+                %{name: "node_count", gatherer: "cluster_node_gatherer", argument: ""}
+              ],
+              values: [
+                %{
+                  name: "fencing_configured",
+                  default: false,
+                  conditions: [
+                    %{value: true, expression: "node_count > 1"}
+                  ],
+                  customization_disabled: false
+                }
+              ],
+              expectations: [
+                %{
+                  name: "fencing_enabled",
+                  type: "expect_enum",
+                  expression: "fencing_configured == true",
+                  failure_message: "Fencing is not configured for all nodes.",
+                  warning_message: "Warning: fencing may not be fully configured."
+                }
+              ],
+              when: "node_count > 0",
+              premium: false,
+              customization_disabled: true
+            }
+          ]
         }
-      },
-      example: %{
-        items: [
-          %{
-            id: "156F64",
-            name: "Corosync `token` timeout",
-            group: "Corosync",
-            description: "Corosync `token` timeout is set to the correct value",
-            remediation:
-              "The value of the Corosync `token` timeout is not set as recommended. Adjust the corosync `token` timeout as recommended...",
-            metadata: %{"category" => "ha", "impact" => "critical"},
-            severity: "critical",
-            facts: [
-              %{name: "node_count", gatherer: "cluster_node_gatherer", argument: ""}
-            ],
-            values: [
-              %{
-                name: "fencing_configured",
-                default: false,
-                conditions: [
-                  %{value: true, expression: "node_count > 1"}
-                ],
-                customization_disabled: false
-              }
-            ],
-            expectations: [
-              %{
-                name: "fencing_enabled",
-                type: "expect_enum",
-                expression: "fencing_configured == true",
-                failure_message: "Fencing is not configured for all nodes.",
-                warning_message: "Warning: fencing may not be fully configured."
-              }
-            ],
-            when: "node_count > 0",
-            premium: false,
-            customization_disabled: true
-          }
-        ]
       }
     },
     struct?: false
