00899405ac50cb601a41617e7acfe698c289a25a
Nelson Kopliku <nelson.kopliku@suse.com>
2025-10-17 10:38:13 +0200

    Use token introspection in Authentication plug

---
+++ test/wanda_web/auth/auth_plug_test.exs
@@ -0,0 +1,100 @@
+defmodule WandaWeb.Auth.AuthPlugTest do
+  use WandaWeb.ConnCase, async: false
+
+  import Mox
+  import Wanda.Factory
+
+  alias WandaWeb.Auth.AuthPlug
+
+  setup [:set_mox_from_context, :verify_on_exit!]
+
+  describe "call/2" do
+    setup do
+      stub(
+        Joken.CurrentTime.Mock,
+        :current_time,
+        fn ->
+          1_671_715_992
+        end
+      )
+
+      :ok
+    end
+
+    test "should return the connection with the user related information" do
+      active_token = Faker.String.base64(32)
+
+      user_id = Faker.Random.Elixir.random_between(1, 100)
+      abilities = build_list(3, :ability)
+
+      expect(WandaWeb.Auth.Client.AuthClientMock, :introspect_token, fn ^active_token ->
+        {:ok,
+         %{
+           active: true,
+           sub: user_id,
+           abilities: abilities
+         }}
+      end)
+
+      conn =
+        build_conn()
+        |> put_req_header("authorization", "Bearer " <> active_token)
+        |> AuthPlug.call([])
+
+      refute conn.halted
+      assert conn.private.user_id == user_id
+
+      assert conn.private.abilities == abilities
+    end
+
+    test "should return the connection with empty user abilities" do
+      active_token = Faker.String.base64(32)
+
+      user_id = Faker.Random.Elixir.random_between(1, 100)
+
+      expect(WandaWeb.Auth.Client.AuthClientMock, :introspect_token, fn ^active_token ->
+        {:ok,
+         %{
+           active: true,
+           sub: user_id,
+           abilities: []
+         }}
+      end)
+
+      conn =
+        build_conn()
+        |> put_req_header("authorization", "Bearer " <> active_token)
+        |> AuthPlug.call([])
+
+      assert conn.private.abilities == []
+    end
+
+    test "should be halted and have unauthorized status if an inactive token was passed" do
+      inactive_token = Faker.String.base64(32)
+
+      expect(WandaWeb.Auth.Client.AuthClientMock, :introspect_token, fn ^inactive_token ->
+        {:ok,
+         %{
+           active: false,
+           sub: Faker.Random.Elixir.random_between(1, 100),
+           abilities: build_list(3, :ability)
+         }}
+      end)
+
+      conn =
+        build_conn()
+        |> put_req_header("authorization", "Bearer " <> inactive_token)
+        |> AuthPlug.call([])
+
+      assert conn.status == 401
+      assert conn.halted
+    end
+
+    test "should be halted and have unauthorized status if no token was passed" do
+      conn = AuthPlug.call(build_conn(), [])
+
+      assert conn.status == 401
+      assert conn.halted
+    end
+  end
+end
