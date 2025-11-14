64b23c3ba55ebe4d409ae17d6ba3e417d6eda467
Nelson Kopliku <nelson.kopliku@suse.com>
2025-11-14 11:37:39 +0100

    Dummy

--- config/config.exs
+++ config/config.exs
@@ -100,3 +100,5 @@ config :wanda, :auth_server, url: "http://localhost:4000"
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
+
+# dummy change
