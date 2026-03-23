7c8029beda58c19a55d597c48eff78caf10cf377
arbulu89 <xarbulu@suse.com>
2026-03-23 15:27:07 +0100

    Update database start/stop to handle multi-tier system replication

--- lib/wanda/operations/catalog/databasestop_v1.ex
+++ lib/wanda/operations/catalog/databasestop_v1.ex
@@ -6,21 +6,36 @@ defmodule Wanda.Operations.Catalog.DatabaseStopV1 do
       id: "databasestop@v1",
       name: "Stop HANA database",
       description: """
-      This operation stops a HANA database. If the database is in a system
-      replication configuration, it only stops the site where the given target
-      belongs to. Other sites are not impacted.
+      This operation stops a HANA database. The database can be a standalone database
+      or a database configured using system replication.
+      The stopping order is from system replication tier 3 to 1.
+      If system replication is not configured it simply stops the given system.
 
       Arguments:
         instance_number (string): Instance number to stop the database
+        system_replication_tier (number): Instance system replication tier number.
+          Null if it does not have system replication
         timeout (number): Timeout in seconds to wait until the database is stopped
       """,
-      required_args: ["instance_number"],
+      required_args: ["instance_number", "system_replication_tier"],
       steps: [
         %Wanda.Operations.Catalog.Step{
-          name: "Stop HANA database",
+          name: "Stop HANA database tier 3",
           operator: "sapsystemstop@v1",
-          predicate: "*",
+          predicate: "system_replication_tier == 3",
           # 12 hours
+          timeout: 12 * 60 * 60 * 1_000
+        },
+        %Wanda.Operations.Catalog.Step{
+          name: "Stop HANA database tier 2",
+          operator: "sapsystemstop@v1",
+          predicate: "system_replication_tier == 2",
+          timeout: 12 * 60 * 60 * 1_000
+        },
+        %Wanda.Operations.Catalog.Step{
+          name: "Stop HANA database tier 1",
+          operator: "sapsystemstop@v1",
+          predicate: "system_replication_tier == 1 || system_replication_tier == ()",
           timeout: 12 * 60 * 60 * 1_000
         }
       ]
