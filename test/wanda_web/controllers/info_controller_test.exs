448c705c617d4e064e1a4e4d3109ce002f46d4f0
balanza <emanuele.decupis@suse.com>
2026-04-03 19:25:50 +0200

    add info controller

---
+++ test/wanda_web/controllers/info_controller_test.exs
@@ -0,0 +1,32 @@
+defmodule WandaWeb.InfoControllerTest do
+  @moduledoc false
+
+  use WandaWeb.ConnCase, async: true
+
+  import OpenApiSpex.TestAssertions
+
+  alias WandaWeb.Schemas.Unversioned.ApiSpec
+
+  setup do
+    %{api_spec: ApiSpec.spec()}
+  end
+
+  describe "Info" do
+    test "returns the current Wanda instance information", %{
+      conn: conn,
+      api_spec: api_spec
+    } do
+      response =
+        conn
+        |> get("/api")
+        |> json_response(200)
+
+      assert %{
+               "name" => "wanda",
+               "version" => _
+             } = response
+
+      assert_schema(response, "InfoV1", api_spec)
+    end
+  end
+end
